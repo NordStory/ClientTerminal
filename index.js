2f855a12f527774c1ccd846d818932998500907b
Valery <88161325+NordStory@users.noreply.github.com>
2022-07-15 17:06:48 +0300

    Markdown для бота

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const app = express();
 const bodyParser = require('body-parser');
 const TelegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
-
-
+const Parser = bodyParser.urlencoded({ extended: false, });
 const bot = new TelegramBot(process.env.TGTOKEN, {polling: true});
+const option = {
+    "parse_mode": "Markdown"
+};
+
 let PORT = 1500;
 app.listen(PORT, function (err) {
     if (err) console.log("Error in server setup")
     console.log(`http://localhost:${PORT}`);
 });
 
-const Parser = bodyParser.urlencoded({ extended: false, });
-
 app.use(express.static('./public'));
-
 
 app.post('/bot', Parser, (req, res) => {
     if (!req.body) return res.sendStatus(400)
     console.log(`${req.body.InventoryID}`);
-    bot.sendMessage(process.env.IDGROUP, `Клиент ждёт на выдачу Invetory ID: ${req.body.InventoryID}`);
+    bot.sendMessage(process.env.IDGROUP, `Клиент ждёт товар \nInvetory ID: *${req.body.InventoryID}*`,option);
     res.redirect('.')
 });
