f8070690e229408418519e12b7a0325515215945
Valery <88161325+NordStory@users.noreply.github.com>
2022-07-15 16:28:33 +0300

    Телеграмм бот
    Оповещение через телеграмм бота

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const TelegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
 
+
+const bot = new TelegramBot(process.env.TGTOKEN, {polling: true});
 let PORT = 1500;
 app.listen(PORT, function (err) {
     if (err) console.log("Error in server setup")
@@ -16,5 +20,6 @@ app.use(express.static('./public'));
 app.post('/bot', Parser, (req, res) => {
     if (!req.body) return res.sendStatus(400)
     console.log(`${req.body.InventoryID}`);
+    bot.sendMessage(process.env.IDGROUP, `Клиент ждёт на выдачу Invetory ID: ${req.body.InventoryID}`);
     res.redirect('.')
 });
